eb08729b3cb7a028f37ded8935c53a23dd0eea7f
Ddridri <ath78@hi.is>
2015-04-12 21:45:36 +0000

    Dæmi 1.3 betra

--- daemi1_3.m
+++ daemi1_3.m
@@ -1,6 +1,6 @@
 % Keyrsluskráin sýnir hvernig nálgunargildið breytist þegar við setjum punktuppsprettur í hægri hliðina.
 X= [0:1/3:2];% Skilgreinum bilið sem við ætlum að nálga á:
-% Búum til tvær mismunandi nálganir; með og án punktuppsprettu:
+% Búum til tvær mismunandi nálganir; með og án punktuppsprettu fyrir fallið u=x*ln(x)-x:
 c=Galerkinadferd(@(x)x,@(x)(1./x),@(x)-2+0*x,X,[0,1],[-1,-2],[0,-2],0,0);
 r=1;% Látum punktuppsprettu með stærð 0.01 koma inn í punkti x=1
 Q=0.01;
