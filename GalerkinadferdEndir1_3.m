5ccd82ada7fc1576ccdc41181d645648a967dbed
Ddridri <ath78@hi.is>
2015-04-12 19:41:10 +0000

    Liður 1.3

---
+++ GalerkinadferdEndir1_3.m
@@ -0,0 +1,12 @@
+%% Hér sést liðurinn úr fallinu Galerkinadferd sem bætir punktuppspretum við hægri hlið.
+
+% Stingum svo inn punktuppsprettum ef þær eru til staðar, en ef engar uppsprettur eru kallaðar með fallinu, sleppir forritið þessum lið.
+if ~isempty(r)
+m=length(r);
+for j=1:m
+    
+    i=max(find(x<=r(j)));
+    b(i)= b(i)+Q(j);
+
+end
+end
